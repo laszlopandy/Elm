dc666d920d83e078673ecbbf699d200bf826d9ce
Laszlo Pandy <laszlo.pandy@prezi.com>
2013-08-09 13:00:23 +0200

    New hastec needs --full-unicode option.

--- haste-build.sh
+++ haste-build.sh
@@ -9,6 +9,6 @@ printf "\nvar __elmDocs__ = " >> all.js
 cat docs.json >> all.js
 popd
 
-hastec --debug JsMain/ElmJsMain.hs -iTransform:Gen:Model:HastePlatform -hide-package monads-tf-0.1.0.1 --with-js=JsMain/all.js
+hastec --debug JsMain/ElmJsMain.hs --full-unicode -iTransform:Gen:Model:HastePlatform -hide-package monads-tf-0.1.0.1 --with-js=JsMain/all.js
 
 popd
